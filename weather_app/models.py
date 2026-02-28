73be399357db6f47e6e5dd6a54aaf69f3f31abb0
zpenr <zpenr@bk.ru>
2026-03-01 00:47:33 +0800

    Create base version of Location table

--- weather_app/models.py
+++ weather_app/models.py
@@ -4,3 +4,9 @@ class Users(models.Model):
     ID = models.IntegerField(primary_key=True)
     Login = models.CharField(max_length=50)
     Password = models.CharField(max_length=50)
+
+class Locations(models.Model):
+    Name = models.CharField(max_length=50)
+    UserId = models.IntegerField()
+    Latitude = models.DecimalField(max_digits=5, decimal_places=2)
+    Longitude = models.DecimalField(max_digits=5, decimal_places=2)
