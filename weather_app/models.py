0e5af4c495110ea022f89b36e136c76376192fcf
zpenr <zpenr@bk.ru>
2026-02-28 23:47:25 +0800

    Create base version of Users table

--- weather_app/models.py
+++ weather_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    Login = models.CharField(max_length=50)
+    Password = models.CharField(max_length=50)
