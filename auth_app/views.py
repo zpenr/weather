4e095fb85b682aa6b1869f2ad888e6cd7f1b5684
zpenr <zpenr@bk.ru>
2026-03-01 18:13:15 +0800

    Create class to login view

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views import View
-
+from auth_app.models import Users
+import bcrypt
 class Login(View):
     def get(self, request):
         return render(request,'auth_app/login.html')
+    
+    def post(self,request):
+        login = request.POST['login']
+        salt = bcrypt.gensalt()
+        password = bcrypt.hashpw(request.POST['password'].encode('utf-8'),salt)
+        user = Users(Login = login, Password = password)
+        user.save()
+        return HttpResponse('Sucsess')
