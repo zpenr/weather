9f4fc4073cf0a540e57cfe5f0eb32f979c8a191c
zpenr <zpenr@bk.ru>
2026-03-01 15:31:37 +0800

    replace model of Users table

--- weather_app/models.py
+++ weather_app/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 
-class Users(models.Model):
-    ID = models.IntegerField(primary_key=True)
-    Login = models.CharField(max_length=50)
-    Password = models.CharField(max_length=50)
-
 class Locations(models.Model):
     Name = models.CharField(max_length=50)
     UserId = models.IntegerField()
