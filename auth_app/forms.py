c46a9a16e7e92f98e1570d34768e0d4cfb6b726c
zpenr <zpenr@bk.ru>
2026-03-03 02:03:01 +0800

    fix password field

--- auth_app/forms.py
+++ auth_app/forms.py
@@ -2,21 +2,32 @@ from django import forms
 
 class LoginForm(forms.Form):
     login = forms.CharField(
-        label='Login', max_length=50,
+        label='Login', 
+        max_length=50,
         widget=forms.TextInput(attrs={'class': 'form-control'})
         )
+    
     password = forms.CharField(
-        label='Password', max_length=50, 
+        label='Password', 
+        max_length=50, 
         widget=forms.PasswordInput(attrs={'class': 'form-control'})
         )
+    
     rep_password = forms.CharField(
-        label='Repeat password', max_length=50, 
+        label='Repeat password', 
+        max_length=50, 
         widget=forms.PasswordInput(attrs={'class': 'form-control'})
         )
 
 class SignUpForm(forms.Form):
     login = forms.CharField(
-        label='Login', max_length=50,
+        label='Login', 
+        max_length=50,
         widget=forms.TextInput(attrs={'class': 'form-control'})
         )
-    password = forms.CharField(label='Password', max_length=50, widget=forms.PasswordInput())
+    
+    password = forms.CharField(
+        label='Password',
+        max_length=50,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+        )
